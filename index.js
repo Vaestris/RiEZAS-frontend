cf002f1d3d4e1905c3ebe2d3e0cbf62b1e17b971
Kirill Seleznev <Kirill.seleznev@sftcomp.ru>
2023-10-11 17:16:59 +0300

    Настроен web server

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
+const fs = require('fs')
 const http = require('http')
+const path = require('path')
+const server = http.createServer((req, res) => {
+
+    let filePath = path.join(__dirname, 'public', req.url == '/' ? 'home.html' : req.url)
+    const ext = path.extname(filePath)
+    let contentType = 'text/html'
+
+    switch(ext) {
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.js':
+            contentType = 'text/javascript'
+            break
+        default:
+            contentType = 'text/html'
+    }
+
+    if (!ext) {
+        filePath += '.html'
+    }
+
+    fs.readFile(filePath, (err, content) => {
+    if(err) {
+        fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) =>{
+            if(err) {
+                res.writeHead(404)
+                res.end("Not found!")
+            } else {
+                res.writeHead(200)
+                res.end(data)
+            }
+        })
+    } else {
+        res.writeHead(200, {
+            'Content-Type': contentType})
+            res.end(content)
+    }
+    })
+})
 
 server.listen(3000, () => {
     console.log('started')
